36ef7da8636c3b2c3e7620e7a20f7e557b523404
f1lander <filanderuclez@gmail.com>
2025-04-22 12:17:38 -0600

    adds redesign

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,67 @@
 import clsx from "clsx";
 import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "../components/HomepageFeatures";
+import React from "react";
 
 import Heading from "@theme/Heading";
 import styles from "./index.module.css";
 
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
+function StripedBackground() {
+  const lines = Array.from({ length: 30 }, (_, i) => (
+    <div key={i} className="striped-line">
+      <div className="striped-line-left"></div>
+      <div className="striped-line-right"></div>
+    </div>
+  ));
+  
+  return <div className="striped-background">{lines}</div>;
+}
+
+function FeaturePills() {
+  const features = [
+    "Oracles",
+    "Prediction Markets", 
+    "AI Inference Engines",
+    "ZK Co-Processors",
+    "Intents Solvers",
+    "Keeper Networks"
+  ];
+  
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
-            Introduction to Catalysis
-          </Link>
+    <div className={styles.featurePillsContainer}>
+      {features.map((feature, idx) => (
+        <div key={idx} className="feature-pill">
+          <span></span> {feature}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function HomepageHeader() {
+  return (
+    <header className={clsx(styles.heroBanner)}>
+      <StripedBackground />
+      <div className={styles.heroContent}>
+        <div className={styles.heroLeft}>
+        </div>
+        <div className={styles.heroRight}>
+          <Heading as="h1" className={styles.title}>
+            Catalysis Network
+          </Heading>
+          <p className={styles.subtitle}>
+            The First Security Abstraction Layer
+          </p>
+          <div className={styles.buttons}>
+            <Link
+              className="start-building-button"
+              to="/docs/intro"
+            >
+              Introduction to Catalysis
+            </Link>
+          </div>
+          <FeaturePills />
         </div>
       </div>
     </header>
@@ -30,13 +69,14 @@ function HomepageHeader() {
 }
 
 export default function Home() {
+
   return (
     <Layout
       title="Catalysis"
       description="The First Security Abstraction Layer"
     >
       <HomepageHeader />
-      <main>
+      <main className="home-main">
         <HomepageFeatures />
       </main>
     </Layout>
